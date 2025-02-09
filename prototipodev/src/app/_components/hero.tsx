de2ef550e52fa983346b109cfa7beda08234ef1c
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2025-02-09 15:47:55 -0300

    Criando section

--- prototipodev/src/app/_components/hero.tsx
+++ prototipodev/src/app/_components/hero.tsx
@@ -1,6 +1,29 @@
 export function Hero() {
   return(
-    <main>
-    </main>
+    <section className="bg-[#E84C3D] text-white relative overflow-hidden">
+
+      <div>
+        <article>
+          <div>
+            <h1>Titulo do site!</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae error nam sequi !</p>
+            <div>
+              <a href="#">
+                Contato WhatsApp
+              </a>
+            </div>
+
+            {/* <div>
+              <a href="#">
+                5% de desconto na primeira compra
+              </a>
+            </div> */}
+
+            
+          </div>
+        </article>
+      </div>
+
+    </section>
   )
 }
